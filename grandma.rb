3ad2099816b81e4dde9dd52bf51e49c53dbc1d5c
Richard Cho <96richardohc@gmail.com>
2020-02-05 07:04:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,3 @@
-speak_to_grandma = gets.chomp
-
-if speak_to_grandma == "Hi Granny".downcase
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "Hi Granny".upcase
-  puts "NO, NOT SINCE 1938!"
-elsif speak_to_grandma == "I love you grandma!".upcase
-  puts "I LOVE YOU TOO PUMPKIN!"
-end
+def speak_to_grandma(phrase)
+  
+  
