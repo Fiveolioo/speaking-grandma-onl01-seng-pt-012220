ebc82e37d43288c57c2199d331adb9e99a08cce3
Richard Cho <96richardohc@gmail.com>
2020-02-05 07:27:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase != phrase.upcase
     "NO, NOT SINCE 1938!"
   else 
     "HUH?! SPEAK UP, SONNY"
   end
 end
  
-# "WHAT DID YOU EAT TODAY?" == "WHAT DID YOU EAT TODAY?"
+# "Hi Nana, how are you?" != "HI NANA, HOW ARE YOU?"
